7b524d4583d78b54cb9aafc69cfe7acdbb8c9c3a
ehsanlatif <ehsanlatif000@gmail.com>
2022-06-09 23:18:14 -0400

    Initial Commit

---
+++ WISE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WISE
+//
+//  Created by Ehsan Latif on 6/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WISE/WISEApp.swift
@@ -0,0 +1,17 @@
+//
+//  WISEApp.swift
+//  WISE
+//
+//  Created by Ehsan Latif on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WISEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
